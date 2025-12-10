f8bf0df9f6826d92cfb118a0e2511e3c88ce7784
GeethaSri09 <nimmadalageethasri@gmail.com>
2025-12-10 11:59:23 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const UserModel = require('./models/Users')
+
+const App = express()
+const port = process.env.PORT || 3001
+App.use(cors())
+App.use(express.json())
+
+mongoose.connect("mongodb://127.0.0.1:27017/CRUD")
+
+App.get("/",(req,res)=>{
+    UserModel.find({})
+    .then(users => res.json(users))
+    .catch(err => res.json(err))
+})
+
+App.get("/getUser/:id",(req,res)=>{
+    const id = req.params.id;
+    UserModel.findById({_id:id})
+    .then(users => res.json(users))
+    .catch(err => res.json(err))
+})
+
+App.put("/updateUser/:id",(req,res)=>{
+    const id = req.params.id;
+    UserModel.findByIdAndUpdate({_id:id},
+         {
+            name:req.body.name, 
+            email:req.body.email,
+            age:req.body.age
+        })
+    .then(users => res.json(users))
+    .catch(err => res.json(err))
+})
+
+App.delete("/deleteUser/:id",(req,res)=>{
+    const id = req.params.id;
+    UserModel.findByIdAndDelete({_id:id})
+    .then(users => res.json(users))
+    .catch(err => res.json(err))
+})
+
+App.post("/createUser",(req,res)=>{
+    UserModel.create(req.body)
+    .then(users => res.json(users))
+    .catch(err => res.json(err))
+})
+
+App.listen(port, () => {
+    console.log("Server is Running")
+})
